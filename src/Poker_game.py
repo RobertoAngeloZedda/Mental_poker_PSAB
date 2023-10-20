4acb94c9724fa560e80dc2354ca7f151133c1891
NicolaLoiZedda <n.l.zedda@gmail.com>
2023-10-21 00:33:17 +0200

    hand evaluation

--- src/Poker_game.py
+++ src/Poker_game.py
@@ -41,27 +41,204 @@ class Poker_game:
             for player in self.players:
                 player.hand.append(self.deck.pop())
 
-    # TODO def evaluate_hand(self, hand):
+    def sort_hand(self, hand):
+        return sorted(hand, key=lambda x: x.rank.value, reverse=True)
     
+    def is_royal_flush(self, hand):
+        return self.is_straight_flush(hand) and (hand[0].rank == Rank.ACE or hand[4].rank == Rank.ACE)
+
+    def is_straight_flush(self, hand):
+        return self.is_straight(hand) and self.is_flush(hand)
+    
+    def is_four_of_a_kind(self, hand): #hand needs to be sorted
+        return hand[0].rank == hand[3].rank or hand[1].rank == hand[4].rank
+    
+    def is_full_house(self, hand): #hand needs to be sorted
+        return (hand[0].rank == hand[2].rank and hand[3].rank == hand[4].rank) or (hand[2].rank == hand[4].rank and hand[0].rank == hand[1].rank)
+        
+    def is_flush(self, hand):
+        return all(card.suit == hand[0].suit for card in hand)
+
+    def is_straight(self, hand):
+        return all(hand[i].rank.value - hand[i+1].rank.value == 1 for i in range(len(hand) - 1)) or all(hand[i].rank.value - hand[i+1].rank.value == -1 for i in range(len(hand) - 1))
+    
+    def is_three_of_a_kind(self, hand): #hand needs to be sorted
+        return hand[0].rank == hand[2].rank or hand[1].rank == hand[3].rank or hand[2].rank == hand[4].rank
+
+    def is_two_pair(self, hand): #hand needs to be sorted
+        return (hand[0].rank == hand[1].rank and hand[2].rank == hand[3].rank) or (hand[0].rank == hand[1].rank and hand[3].rank == hand[4].rank) or (hand[1].rank == hand[2].rank and hand[3].rank == hand[4].rank)
+
+    def is_one_pair(self, hand): #hand needs to be sorted
+        return any(hand[i].rank == hand[i+1].rank for i in range(len(hand) - 1))
+    
+    def order_four_of_a_kind_hand(self, hand):
+        ordered_hand = []
+        rank_counts = {rank: [] for rank in Rank}
+
+        for card in hand:
+            rank_counts[card.rank].append(card)
+        
+        for rank in Rank:
+            if len(rank_counts[rank]) == 4:
+                ordered_hand.insert(0, rank_counts[rank][0])
+                ordered_hand.insert(0, rank_counts[rank][1])
+                ordered_hand.insert(0, rank_counts[rank][2])
+                ordered_hand.insert(0, rank_counts[rank][3])
+            elif len(rank_counts[rank]) == 1:
+                ordered_hand.append(rank_counts[rank][0])
+
+        return ordered_hand
+    
+    def order_full_house_hand(self, hand):
+        ordered_hand = []
+        rank_counts = {rank: [] for rank in Rank}
+
+        for card in hand:
+            rank_counts[card.rank].append(card)
+        
+        for rank in Rank:
+            if len(rank_counts[rank]) == 3:
+                ordered_hand.insert(0, rank_counts[rank][0])
+                ordered_hand.insert(0, rank_counts[rank][1])
+                ordered_hand.insert(0, rank_counts[rank][2])
+            elif len(rank_counts[rank]) == 2:
+                ordered_hand.append(rank_counts[rank][0])
+                ordered_hand.append(rank_counts[rank][1])
+
+        return ordered_hand
+
+    def order_three_of_a_kind_hand(self, hand):
+        ordered_hand = []
+        rank_counts = {rank: [] for rank in Rank}
+
+        for card in hand:
+            rank_counts[card.rank].append(card)
+        
+        for rank in Rank:
+            if len(rank_counts[rank]) == 3:
+                ordered_hand.insert(0, rank_counts[rank][0])
+                ordered_hand.insert(0, rank_counts[rank][1])
+                ordered_hand.insert(0, rank_counts[rank][2])
+            elif len(rank_counts[rank]) == 1:
+                ordered_hand.append(rank_counts[rank][0])
+
+        tmp = ordered_hand[3]
+        ordered_hand[3] = ordered_hand[4]
+        ordered_hand[4] = tmp
+        
+        return ordered_hand
+
+    def order_two_pair_hand(self, hand):
+        ordered_hand = []
+        rank_counts = {rank: [] for rank in Rank}
+
+        for card in hand:
+            rank_counts[card.rank].append(card)
+
+        for rank in Rank:
+            if len(rank_counts[rank]) == 2:
+                ordered_hand.insert(0, rank_counts[rank][0])
+                ordered_hand.insert(0, rank_counts[rank][1])
+            elif len(rank_counts[rank]) == 1:
+                ordered_hand.append(rank_counts[rank][0])
+        
+        return ordered_hand
+
+    def order_one_pair_hand(self, hand):
+        ordered_hand = []
+        rank_counts = {rank: [] for rank in Rank}
+
+        for card in hand:
+            rank_counts[card.rank].append(card)
+        
+        for rank in Rank:
+            if len(rank_counts[rank]) == 2:
+                ordered_hand.insert(0, rank_counts[rank][0])
+                ordered_hand.insert(0, rank_counts[rank][1])
+            elif len(rank_counts[rank]) == 1:
+                ordered_hand.append(rank_counts[rank][0])
+        
+        tmp = ordered_hand[2]
+        ordered_hand[2] = ordered_hand[4]
+        ordered_hand[4] = tmp
+
+    def evaluate_hand(self, hand):
+        sorted_hand = self.sort_hand(hand)
+
+        if self.is_royal_flush(hand):
+            return (Hand_Ranking.ROYALFLUSH, sorted_hand)
+
+        if self.is_straight_flush(hand):
+            return (Hand_Ranking.STRAIGHTFLUSH, sorted_hand)
+        
+        if self.is_four_of_a_kind(sorted_hand):
+            return (Hand_Ranking.FOUROFAKIND, self.order_four_of_a_kind_hand(sorted_hand))
+        
+        if self.is_full_house(sorted_hand):
+            return (Hand_Ranking.FULLHOUSE, self.order_full_house_hand(sorted_hand))
+        
+        if self.is_flush(sorted_hand):
+            return (Hand_Ranking.FLUSH, sorted_hand)
+        
+        if self.is_straight(hand):
+            return (Hand_Ranking.STRAIGHT, sorted_hand)
+        
+        if self.is_three_of_a_kind(sorted_hand):
+            return (Hand_Ranking.THREEOFAKIND, self.order_three_of_a_kind_hand(sorted_hand))
+        
+        if self.is_two_pair(sorted_hand):
+            return (Hand_Ranking.TWOPAIR, self.order_two_pair_hand(sorted_hand))
+        
+        if self.is_one_pair(sorted_hand):
+            return (Hand_Ranking.ONEPAIR, self.order_one_pair_hand(sorted_hand))
+        
+        return (Hand_Ranking.HIGHCARD, sorted_hand)
+    
+    def compare_ordered_hands(self, hand1, hand2):
+        if hand1 == hand2:
+            return None
+        else:
+            for i in range(5):
+                if hand1[i].rank.value > hand2[i].rank.value:
+                    return hand1
+                if hand1[i].rank.value < hand2[i].rank.value:
+                    return hand2
+    
+    def hand_results(self):
+        winning_player = None
+        best_hand = None
+
+        for player in self.players:
+            player.show_hand()
+            evaluated_hand = self.evaluate_hand(player.hand)
+            print(f"Hand ranking: {hand_ranking_dict[evaluated_hand[0]]}\n")
+            
+            if best_hand is None or evaluated_hand[0].value > best_hand[0].value:
+                best_hand = evaluated_hand
+                winning_player = player
+            elif evaluated_hand[0] == best_hand[0]:
+                # if hand rank is equal, compare value of cards 
+                higher_valued_hand = self.compare_ordered_hands(best_hand[1], evaluated_hand[1])
+                if higher_valued_hand == evaluated_hand[1]:
+                    winning_player = player
+                    best_hand = evaluated_hand
+                elif higher_valued_hand == None:
+                    winning_player == None
+                    best_hand = None
+    
+        return (winning_player, best_hand)
+
     def play(self):
         self.print_players()
         
-        best_hand = None
-        winning_player = None
-        
         self.deal_cards()
-        
-        #for player in self.players:
-        #    evaluated_hand = self.evaluate_hand(player.hand)
-        #    
-        #    if best_hand is None or evaluated_hand > best_hand:
-        #        best_hand = evaluated_hand
-        #        winning_player = player
+
+        (winning_player, best_hand) = self.hand_results()
 
         if winning_player != None:
-            print(f"WINNER: {winning_player.name}")
+            print(f"WINNER: {winning_player.name}\n{hand_ranking_dict[best_hand[0]]}")
         else:
             print("DRAW")
         
-        for player in self.players:
-            player.show_hand()
+        #for player in self.players:
+        #    player.show_hand()
