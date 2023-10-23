1eb94a3294a5fb9e2b13f91112f1cf1a044d5673
RobertoAngeloZedda <xrhayux@gmail.com>
2023-10-23 19:53:03 +0200

    draw

--- src/Client.py
+++ src/Client.py
@@ -1,4 +1,5 @@
 from Contract_communication_handler import *
+from SRA import *
 
 cch = Contract_communication_handler(addresses_file_path='./addresses.txt', 
                                      abi_file_path='./abi.json',
@@ -9,7 +10,69 @@ cch.participate()
 assigned_index = cch.get_my_turn_index()
 print('Assigned index:', assigned_index)
 
-print('Listening for events')
-cch.catch_event(1, 0)
+print('Listening for shuffle events')
+cch.catch_shuffle_event(assigned_index)
 
-print('Can start shuffling..')
+# If client is dealer he gotta shuffle the deck
+if assigned_index == 0:
+    n = sra_setup(32)
+    print('n =', n)
+
+    # Choosing only quadratic residues to map the cards to represent the deck
+    deck_coding = []
+    deck_size = 52
+    count = 0
+    for i in range(2, n):
+        if count >= deck_size:
+            break
+        if is_quadratic_residue(i, n) == 1:
+            deck_coding.append(i)
+            count += 1
+    print('deck coding =\n', deck_coding)
+
+    e, d = sra_generate_key(n-1)
+
+    #shuffle
+
+    enc = [sra_encrypt(card, e, n) for card in deck_coding]
+    print('encrypted_deck =\n', enc)
+
+    cch.shuffle_dealer(n, deck_coding, enc)
+
+# If client is not dealer (he reads n and deck coding)
+else:
+    n = cch.get_n()
+    print('n =', n)
+
+    deck_coding = cch.get_deck_coding()
+    print('deck coding =\n', deck_coding)
+
+    deck = cch.get_encrypted_deck()
+
+    e, d = sra_generate_key(n-1)
+
+    #shuffle
+
+    enc = [sra_encrypt(card, e, n) for card in deck]
+    print('encrypted_deck =\n', enc)
+
+    cch.shuffle(enc)
+
+print('Listening for draw events')
+draw_index, topdeck_index, hand_size = cch.catch_draw_event(assigned_index)
+
+deck = cch.get_encrypted_deck()
+
+encrypted_hand = deck[topdeck_index : (topdeck_index + hand_size)]
+
+hand = [sra_decrypt(card, d, n) for card in encrypted_hand]
+
+print(hand)
+
+# If client has to draw
+if draw_index == assigned_index:
+    cch.draw()
+
+# If someoneelse has to draw
+else:
+    cch.reveal_cards(hand)
--- src/Contract_communication_handler.py
+++ src/Contract_communication_handler.py
@@ -58,15 +58,82 @@ class Contract_communication_handler:
 			print('Error during the creation of the "Contract" object.')
 			exit()
 
+	def catch_shuffle_event(self, turn_index):
+
+		# check if my last transaction triggered the event
+		transaction = self.connection.eth.get_transaction(self.last_transaction)
+		block_number = transaction['blockNumber']
+		logs = self.contract.events.shuffle_event.get_logs(fromBlock=block_number)
+		if len(logs) >= 1:
+			_turn_index = logs[-1]['args']['turn_index']
+
+			print('Past event caught (from block', block_number, '). {turn_index:', _turn_index, '}')
+			if _turn_index == turn_index:
+				return
+		
+		# otherwise listen for it
+		event_filter = self.contract.events.shuffle_event.create_filter(fromBlock='latest')
+
+		while True:
+			for event in event_filter.get_new_entries():
+				_turn_index = event['args']['turn_index']
+
+				print('New event caught. {turn_index:', _turn_index, '}')
+				if _turn_index == turn_index:
+					return
+				
+			time.sleep(1)
+	
+	def catch_draw_event(self, turn_index):
+
+		# check if my last transaction triggered the event
+		transaction = self.connection.eth.get_transaction(self.last_transaction)
+		block_number = transaction['blockNumber']
+		logs = self.contract.events.draw_event.get_logs(fromBlock=block_number)
+		if len(logs) >= 1:
+			_turn_index = logs[-1]['args']['turn_index']
+			draw_index = logs[-1]['args']['draw_index']
+			topdeck_index = logs[-1]['args']['topdeck_index']
+			hand_size = logs[-1]['args']['hand_size']
+
+			print('Past event caught (from block', block_number, ').',
+		          ' {turn_index:', _turn_index,
+		 		  ', draw_index:', draw_index,
+				  ', topdeck_index:', topdeck_index,
+				  ', hand_size:', hand_size, '}')
+			if _turn_index == turn_index:
+				return draw_index, topdeck_index, hand_size
+		
+		# otherwise listen for it
+		event_filter = self.contract.events.draw_event.create_filter(fromBlock='latest')
+
+		while True:
+			for event in event_filter.get_new_entries():
+				_turn_index = event['args']['turn_index']
+				draw_index = event['args']['draw_index']
+				topdeck_index = event['args']['topdeck_index']
+				hand_size = event['args']['hand_size']
+
+				print('New event caught.',
+		              ' {turn_index:', _turn_index,
+					  ', draw_index:', draw_index,
+					  ', topdeck_index:', topdeck_index,
+					  ', hand_size:', hand_size, '}')
+				if _turn_index == turn_index:
+					return draw_index, topdeck_index, hand_size
+				
+			time.sleep(1)
+
+
 	def transact(self, wei_amount: int):
-			try:
-				self.connection.eth.send_transaction(
-					{'from': self.wallet_address,
-					'to': self.contract_address,
-					'value': wei_amount})
-			except:
-				print('Error during the transaction.')
-				exit()
+		try:
+			self.connection.eth.send_transaction(
+				{'from': self.wallet_address,
+				'to': self.contract_address,
+				'value': wei_amount})
+		except:
+			print('Error during the transaction.')
+			exit()
 
 	def participate(self):
 		try:
@@ -79,31 +146,46 @@ class Contract_communication_handler:
 			return self.contract.functions.get_my_turn_index().call({'from': self.wallet_address})
 		except:
 			exit('Error while calling function "get_my_turn_index".')
-
-	def catch_event(self, status, turn_index):
-
-		# check if my last transaction triggered the event
-		transaction = self.connection.eth.get_transaction(self.last_transaction)
-		block_number = transaction['blockNumber']
-		logs = self.contract.events.update.get_logs(fromBlock=block_number)
-		if len(logs) >= 1:
-			s = logs[-1]['args']['s']
-			i = logs[-1]['args']['i']
-
-			print('Past event caught.\nStatus:', s, '\nTurn_index:', i)
-			if s == status and i == turn_index:
-				return
-		
-		# otherwise listen for it
-		event_filter = self.contract.events.update.create_filter(fromBlock='latest')
-
-		while True:
-			for event in event_filter.get_new_entries():
-				s = event['args']['s']
-				i = event['args']['i']
-
-				print('New event caught.\nStatus:', s, '\nTurn_index:', i)
-				if s == status and turn_index == i:
-					return
-				
-			time.sleep(1)
+	
+	def shuffle_dealer(self, n, deck_coding, encrypted_deck):
+		try:
+			self.last_transaction = self.contract.functions.shuffle_dealer(n, deck_coding, encrypted_deck).transact({'from': self.wallet_address})
+		except:
+			print('Error while calling function "shuffle_dealer".')
+			exit()
+	
+	def get_n(self):
+		try:
+			return self.contract.functions.get_n().call({'from': self.wallet_address})
+		except:
+			exit('Error while calling function "get_n".')
+	
+	def get_deck_coding(self):
+		try:
+			return self.contract.functions.get_deck_coding().call({'from': self.wallet_address})
+		except:
+			exit('Error while calling function "get_deck_coding".')
+	
+	def get_encrypted_deck(self):
+		try:
+			return self.contract.functions.get_encrypted_deck().call({'from': self.wallet_address})
+		except:
+			exit('Error while calling function "get_encrypted_deck".')
+	
+	def shuffle(self, encrypted_deck):
+		try:
+			self.last_transaction = self.contract.functions.shuffle(encrypted_deck).transact({'from': self.wallet_address})
+		except:
+			exit('Error while calling function "shuffle".')
+	
+	def reveal_cards(self, encrypted_cards):
+		try:
+			self.last_transaction = self.contract.functions.reveal_cards(encrypted_cards).transact({'from': self.wallet_address})
+		except:
+			exit('Error while calling function "reveal_cards".')
+	
+	def draw(self):
+		try:
+			self.last_transaction = self.contract.functions.draw().transact({'from': self.wallet_address})
+		except:
+			exit('Error while calling function "draw".')
